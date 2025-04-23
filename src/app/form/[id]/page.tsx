cf13b8e8f6b2c5aa8e32458e47380a2795925855
tiojimbo <isamuelmacedo0@gmail.com>
2025-04-23 00:57:01 -0300

    fix: page.tsx compatibility with Next.js App Router

--- src/app/form/[id]/page.tsx
+++ src/app/form/[id]/page.tsx
@@ -1,7 +1,17 @@
-export default function FormPage({ params }: { params: { id: string } }) {
+import { FC } from "react";
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+const FormPage: FC<PageProps> = ({ params }) => {
   return (
     <div className="p-6 text-center text-lg font-medium">
       Viewing Form ID: <strong>{params.id}</strong>
     </div>
   );
-}
+};
+
+export default FormPage;
