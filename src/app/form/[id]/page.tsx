cf489a4039ae7c176f83be946a3b76c91801610e
tiojimbo <isamuelmacedo0@gmail.com>
2025-04-23 02:48:22 -0300

    fix: adjust params typing for Next.js 15

--- src/app/form/[id]/page.tsx
+++ src/app/form/[id]/page.tsx
@@ -1,12 +1,7 @@
-export default function FormPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function FormPage({ params }: { params: { id: string } }) {
   return (
     <div className="p-6 text-center text-lg font-medium">
       Viewing Form ID: <strong>{params.id}</strong>
     </div>
   );
 }
- //
