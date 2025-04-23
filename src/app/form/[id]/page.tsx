95bff5f828924c2efc30e1afaa7f860c4bebfba7
tiojimbo <isamuelmacedo0@gmail.com>
2025-04-23 00:12:54 -0300

    fix: form page type error

--- src/app/form/[id]/page.tsx
+++ src/app/form/[id]/page.tsx
@@ -1,4 +1,10 @@
-export default function FormPage({ params }: { params: { id: string } }) {
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+export default function FormPage({ params }: Params) {
   return (
     <div className="p-6 text-center text-lg font-medium">
       Viewing Form ID: <strong>{params.id}</strong>
